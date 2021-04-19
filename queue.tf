284bab33f06e8e0d339b14620d8bd49226252c9f
whuandra <50607920+whuandra@users.noreply.github.com>
2021-04-18 19:16:34 -0700

    Update queue.tf

--- queue.tf
+++ queue.tf
@@ -10,3 +10,22 @@ resource "aws_sqs_queue" "win_queue" {
     environment = "dev"
     }
   }
+resource "aws_sqs_queue_policy" "test" {
+  queue_url = aws_sqs_queue.win_queue.id
+  
+  policy = <<POLICY
+{
+"Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.win_queue.arn}",
+    }
+  ]
+}
+POLICY
+}
