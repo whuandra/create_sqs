2492c350de9c8198292a31cf19b2e7f5ad57c5dc
whuandra <50607920+whuandra@users.noreply.github.com>
2021-12-01 21:48:30 -0800

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -5,3 +5,12 @@ resource "aws_iam_user" "aws_aci" {
 resource "aws_iam_group" "aws_aci_group" { 
   name = "aws_aci_Group"
 }
+
+resource "aws_iam_group_membership" "aws_aci_membership" { 
+  name  = "ACIGroupMembership"
+  group = aws_iam_group.aws_aci_group.name
+  users = [
+    aws_iam_user.aws_aci.name
+  ]
+}
+
